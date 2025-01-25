b39e21678a32bd768b877957ec011cfd0454bb88
indu.bala <indu.bala@ihsmarkit.com>
2025-01-25 21:17:17 +0530

    Added model class and static dataStore to return data

---
+++ CityInfo.API/CitiesDataStore.cs
@@ -0,0 +1,32 @@
+﻿using CityInfo.API.Model;
+
+namespace CityInfo.API
+{
+    public class CitiesDataStore
+    {
+
+        public List<CityDTO> Cities { get; set; }
+
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+        public CitiesDataStore()
+        {
+            //init dummy Data
+            Cities = new List<CityDTO>() {
+
+            new CityDTO(){
+               Id = 1,
+                Name = "Shimla",
+                 Description="Queen of hills"
+            },new CityDTO(){
+               Id = 1,
+                Name = "Darjeeling",
+                 Description="Land of tea"
+            },new CityDTO(){
+               Id = 1,
+                Name = "Chirapunjee",
+                 Description="Land where it rains a lot!!"
+            }
+        };
+        }
+    }
+}
--- CityInfo.API/Controllers/CitiesController.cs
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -9,13 +9,8 @@ namespace CityInfo.API.Controllers
         [HttpGet("api/cities")]
         public JsonResult GetCities()
         {
-            return new JsonResult(new List<object> {
-       new { name= "Agra",id=1 },
-       new { name= "Cheerapunji",id=2 },
-        new { name= "Shimla",id=3 },
-
-        });
-
+            return new JsonResult(
+                CitiesDataStore.Current.Cities);
         }
     }
 }
