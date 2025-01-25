87bd5f70b4b1955ed2635d7e91838b7d46f44be7
indu.bala <indu.bala@ihsmarkit.com>
2025-01-25 20:19:35 +0530

    Add project files.

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+
+    [ApiController]
+    public class CitiesController : ControllerBase
+    {
+        [HttpGet("api/cities")]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(new List<object> {
+       new { name= "Agra",id=1 },
+       new { name= "Cheerapunji",id=2 },
+        new { name= "Shimla",id=3 },
+
+        });
+
+        }
+    }
+}
